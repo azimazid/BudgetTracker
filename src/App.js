18fe32c89c06e9fa44d522367b3192d1aaa85436
Azim Azidin <azimazid@gmail.com>
2022-07-15 08:02:32 +0800

    added closing budget modal

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
-import { Button, Stack } from "react-bootstrap";
+import { Button, Modal, Stack } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import AddBudgetModal from "./components/AddBudgetModal";
 import BudgetCard from "./components/BudgetCard";
 
 function App() {
+  const [showAddBudgetModal, setShowAddBudgetModal] = useState(false)
+
   return (
-    <Container className="my-4">
-      <Stack direction="horizontal" gap="2" className="mb-4">
-        <h1 className="me-auto">Budgets</h1>
-        <Button variant="primary">Add Budget</Button>
-        <Button variant="outline-primary">Add Expense</Button>
-      </Stack>
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
-          gap: "1rem",
-          alignItems: "flex-start",
-        }}
-      >
-        <BudgetCard 
-        name="Entertainment"
-        amount={1100} 
-        max={1000}
-        ></BudgetCard>
-      </div>
-    </Container>
-    <AddBudgetModal show />
+    <>
+      <Container className="my-4">
+        <Stack direction="horizontal" gap="2" className="mb-4">
+          <h1 className="me-auto">Budgets</h1>
+          <Button variant="primary" onClick={() => setShowAddBudgetModal(true)}>Add Budget</Button>
+          <Button variant="outline-primary">Add Expense</Button>
+        </Stack>
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+            gap: "1rem",
+            alignItems: "flex-start",
+          }}
+        >
+          <BudgetCard
+            name="Entertainment"
+            amount={1100}
+            max={1000}
+          ></BudgetCard>
+        </div>
+      </Container>
+      <AddBudgetModal show={showAddBudgetModal} handleClose={() => setShowAssBudgetModal(false)}/>
+    </>
   );
 }
 
