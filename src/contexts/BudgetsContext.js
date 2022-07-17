2a63892ff8ba457e43f1a579e4296e0efca0a31a
Azim Azidin <azimazid@gmail.com>
2022-07-18 07:32:15 +0800

    addBudget and add Expense was added

--- src/contexts/BudgetsContext.js
+++ src/contexts/BudgetsContext.js
@@ -4,43 +4,29 @@ import { v4 as uuidV4 } from "uuid";
 
 const BudgetsContext = React.createContext();
 
+export const UNCATEGORIZED_BUDGET_ID = "Uncategorized"
+
 export function useBudgets() {
   return useContext(BudgetsContext);
 }
-
-/*
- How budget will look like
-{
-  id: 
-  name:
-  max
-}
-
- How expenses will look like:
-{
-  id:
-  budgetId:
-  amount:
-  description:
-}*/
 
 export const BudgetsProvider = ({ children }) => {
   const [budgets, setBudgets] = useLocalStorage("budgets", []);
   const [expenses, setExpenses] = useLocalStorage("expenses", []);
 
   function getBudgetExpenses(budgetId) {
-    return expenses.filter((expense) => expense.budgetId === budgetId);
+    return expenses.filter(expense => expense.budgetId === budgetId);
   }
 
-  function addExpense(description, amount, budgetId) {
-    setExpenses((prevExpenses) => {
+  function addExpense({description, amount, budgetId}) {
+    setExpenses(prevExpenses => {
       return [...prevExpenses, { id: uuidV4(), description, amount, budgetId }];
     });
   }
 
-  function addBudget(name, max) {
-    setBudgets((prevBudgets) => {
-      if (prevBudgets.find((budget) => budget.name === name)) {
+  function addBudget({name, max}) {
+    setBudgets(prevBudgets => {
+      if (prevBudgets.find(budget => budget.name === name)) {
         return prevBudgets;
       }
       return [...prevBudgets, { id: uuidV4(), name, max }];
